d0de61c94affc6d734e1d5e75f6b5880690caf2d
HamadUllah <hamadullah16@gmail.com>
2025-09-08 23:38:38 +0500

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
+export default function Home() {
+  return (
+    <Container className="bg-red-300">
+      <Button variant="contained">Click me</Button>
+    </Container>
+  );
+}
